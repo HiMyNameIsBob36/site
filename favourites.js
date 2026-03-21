145a1fdc591c27544b72a532b8da4cd045e46823
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-22 02:03:45 +1100

    Update favourites.js

--- favourites.js
+++ favourites.js
@@ -1,6 +1,6 @@
 /* --- NOTIFICATION & ALERTS SYSTEM --- */
 
-const UPDATES_FILE = './updates.json';
+const UPDATES_FILE = '../updates.json';
 let followed = JSON.parse(localStorage.getItem('followedProjects')) || [];
 let seenUpdates = JSON.parse(localStorage.getItem('seenUpdates')) || [];
 let clickedUpdates = JSON.parse(localStorage.getItem('clickedUpdates')) || [];
@@ -22,7 +22,7 @@ function createToastContainer() {
 
 async function checkForUpdates() {
     try {
-        const response = await fetch(updates.json + '?t=' + Date.now()); // Prevent caching
+        const response = await fetch(../updates.json + '?t=' + Date.now()); // Prevent caching
         const allUpdates = await response.json();
         const container = document.getElementById('toast-container');
 
